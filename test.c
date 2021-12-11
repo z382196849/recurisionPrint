617de76dbaed8c78b5e78d42bb5293a3b49829cf
z382196829 <1936851315@qq.com>
2021-12-11 23:48:35 +0800

    第二十二次提交

---
+++ test.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<windows.h>
+
+int Print(int num)
+{
+    if(num > 0)
+    {
+	Print(num / 10);
+	printf("%d\n", num % 10);
+    }
+}
+
+int main()
+{
+    int i = 0;
+    printf("请输入:i = ");
+    scanf("%d", &i);
+    Print(i);
+    printf("\n");
+    system("pause");
+    return;
+}
